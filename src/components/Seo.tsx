8485754224a035a33d322b1ef107a2a5f9a2a103
naoma121 <naomina@me.com>
2025-01-16 10:06:48 +0900

    SEO関連

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -25,7 +25,7 @@ const Seo: FC<MetaData> = ({
   const title = pageTitle ? `${pageTitle} | ${defaultTitle}` : defaultTitle
   const description = pageDescription ? pageDescription : defaultDescription
   const url = pagePath
-  const imgUrl = pageImg
+  const imgUrl = pageImg? pageImg : `${siteConfig.siteUrl}ogp.jpg`
   const imgWidth = pageImgWidth ? pageImgWidth : 1280
   const imgHeight = pageImgHeight ? pageImgHeight : 640
 
@@ -34,6 +34,7 @@ const Seo: FC<MetaData> = ({
       <title>{title}</title>
       <meta name='viewport' content='width=device-width,initial-scale=1.0' />
       <meta name='description' content={description} />
+      <meta name="thumbnail" content={imgUrl} />
       <meta property='og:url' content={url} />
       <meta property='og:title' content={title} />
       <meta property='og:site_name' content={title} />
@@ -43,6 +44,9 @@ const Seo: FC<MetaData> = ({
       <meta property='og:image:width' content={String(imgWidth)} />
       <meta property='og:image:height' content={String(imgHeight)} />
       <meta name='twitter:card' content='summary_large_image' />
+      <meta name="twitter:image" content={imgUrl} />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
       <link rel='preconnect' href='https://fonts.gstatic.com' />
       <link rel='canonical' href={url} />
     </Head>
